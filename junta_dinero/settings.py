88f3d9ef493cb4d2f1d586d30216266c82271055
yisuscode1 <r.alfaro1882@gmail.com>
2025-10-25 00:46:22 -0500

    se hizo conexion base de datos mysql

--- junta_dinero/settings.py
+++ junta_dinero/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'junta_dinero.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'junta_chain_db',
+        'USER': 'juntachain_user',
+        'PASSWORD': 'TuContraseñaSegura123',  # usa la contraseña que definiste
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
